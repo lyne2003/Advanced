a22c1acaaa2bbce96d7ba28732be095ecd983663
Charbel HAJJMOUSSA <charbelhajjmoussa7@gmail.com>
2024-12-12 18:28:22 +0200

    done add category

--- AdvancedBE/obj/Debug/net8.0/AdvancedBE.AssemblyInfo.cs
+++ AdvancedBE/obj/Debug/net8.0/AdvancedBE.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a668d8a0fd7d4c73d0e1f474e42a8ac83b34bce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e759932142ebaa4bcac807df6d2c61bf4c9f4877")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
