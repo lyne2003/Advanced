e46d054d404c9de1a5b35c7e3d24e8cf4e5a5cb4
Charbel HAJJMOUSSA <charbelhajjmoussa7@gmail.com>
2024-12-16 21:30:27 +0200

    start by admin done client

--- AdvancedBE/obj/Debug/net8.0/AdvancedBE.AssemblyInfo.cs
+++ AdvancedBE/obj/Debug/net8.0/AdvancedBE.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bacf917ba0dd4196f6be4ae4c17123a0ab2d216a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e7b425e4088519fc48e748a20d006af34da793")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
